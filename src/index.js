2ebe841e4f996ba59b8951ce05ef9d4856934fac
aluno <aluno@ifpe>
2024-07-24 15:44:52 -0300

    adicionando core-ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import '@coreui/coreui/dist/css/coreui.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
