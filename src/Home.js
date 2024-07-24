e8f78c4a6ac98d9da6eae38e15b28d88dc6f2fd9
aluno <aluno@ifpe>
2024-07-24 10:38:35 -0300

    visual da home completa

---
+++ src/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Home.css';
+import logo from './logo.png';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="left-section">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="title">
+          <span>A</span>
+          <span>COZINHA</span>
+          <span>FOLHADA</span>
+        </div>
+        <div className="description">
+          <span>COMIDA FEITA COM INGREDIENTES À</span>
+          <span>BASE DE PLANTAS E MUITO AMOR</span>
+        </div>
+      </div>
+      <div className="right-section">
+        <div className="buttons">
+          <button>Cardápio</button>
+          <button>Pratos da Estação</button>
+          <button>O Espaço</button>
+          <button>FAQ de Delivery</button>
+          <button>Sobre Nós</button>
+          <button>Nossa Luta</button>
+        </div>
+        <div className="social-icons">
+          <i className="fab fa-facebook"></i>
+          <i className="fab fa-instagram"></i>
+          <i className="fab fa-twitter"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
